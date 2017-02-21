ca6048257a86dd0d6d2b1656b9a13f5d8707acd8
carlos <carlosph816@gmail.com>
2017-02-21 11:11:22 -0600

    LocationListener
    Obtención de localización en mapa con punto medio.

--- TaxiUsuario/app/src/main/java/com/taxiusuario/Register.java
+++ TaxiUsuario/app/src/main/java/com/taxiusuario/Register.java
@@ -8,7 +8,7 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+        setContentView(R.layout.activity_register);
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
